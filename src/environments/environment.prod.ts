3b2ed7f4f0cf112bc630a811618be4dde88f9d3e
Nicolas MARTY <marty.nicolas11@laposte.net>
2020-09-02 12:01:43 +0200

    NMA: Fix liens

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     backendUrl: `https://pro-cv-api.herokuapp.com`,
-    startURIStaticResources: '/pro-cv-angular/'
+    startURIStaticResources: '/pro-cv-angular'
 };
